695895dffbac070f78839ceb1cad81b78f65461b
Nassim YAHIA <naxwssx95100@gmail.com>
2025-06-26 15:24:15 +0200

    Add files via upload

---
+++ lightbox.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".zoomable").forEach(img => {
+img.addEventListener('click', function(){
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById ('lightbox-img');
+lightbox.style.display='block';
+lightboxImg.src = this.src;
+});
+});
+document.querySelector ('.lightbox .close').addEventListener('click', function() {
+document.getElementById('lightbox').style.display = 'none';
+});
